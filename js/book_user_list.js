ac2423a8dd51d7ebaaaddce4982d95a916752cc4
Soeun_Jang <azulcielo_@naver.com>
2023-05-14 22:37:14 +0900

    commit

--- js/book_user_list.js
+++ js/book_user_list.js
@@ -1,60 +1,60 @@
 checkAdmin()
 // 기본 URL
-const backend_base_url = "http://ec2-3-39-193-171.ap-northeast-2.compute.amazonaws.com:8000"
+const backend_base_url = "http://127.0.0.1:8000"
 const frontend_base_url = "http://127.0.0.1:5500"
 
 async function GetRoom() {
-    const accessToken = localStorage.getItem('access')
-    const response = await fetch(`${backend_base_url}/manager/rooms/`, {
-        headers: {
-            'content-type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`,
-        },
-        method: 'GET',
-    })
-    const response_json = await response.json()
-    response_json.forEach((a) => {
-        let id = a['id']
-        let name = a['name']
-        let temp_html = `<option value="${id}">${name}</option>`
-        $('#select_room').append(temp_html)
-    })
-    // 
+  const accessToken = localStorage.getItem('access')
+  const response = await fetch(`${backend_base_url}/manager/rooms/`, {
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+    },
+    method: 'GET',
+  })
+  const response_json = await response.json()
+  response_json.forEach((a) => {
+    let id = a['id']
+    let name = a['name']
+    let temp_html = `<option value="${id}">${name}</option>`
+    $('#select_room').append(temp_html)
+  })
+  // 
 }
 
 
 
 async function bookUserList() {
-    const accessToken = localStorage.getItem('access')
-    const room_id = document.getElementById("select_room").value
-    const response = await fetch(`${backend_base_url}/manager/customers/${room_id}/`, {
-        headers: {
-            'content-type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`,
-        },
-        method: 'GET',
-    })
+  const accessToken = localStorage.getItem('access')
+  const room_id = document.getElementById("select_room").value
+  const response = await fetch(`${backend_base_url}/manager/customers/${room_id}/`, {
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+    },
+    method: 'GET',
+  })
 
-    const response_json = await response.json()
-    console.log(response_json)
-    $('#book_info').empty()
-    let num = 1
-    response_json['book_set'].forEach((a) => {
-        const check_in = a['check_in']
-        const check_out = a['check_out']
-        const username = a['user_set']['username']
-        const phone = a['user_set']['phone']
+  const response_json = await response.json()
+  console.log(response_json)
+  $('#book_info').empty()
+  let num = 1
+  response_json['book_set'].forEach((a) => {
+    const check_in = a['check_in']
+    const check_out = a['check_out']
+    const username = a['user_set']['username']
+    const phone = a['user_set']['phone']
 
-        let temp_html = `<tr>
+    let temp_html = `<tr>
                                 <th>${num}</th>
                                 <td>${username}</td>
                                 <td>${phone}</td>
                                 <td>${check_in}</td>
                                 <td>${check_out}</td>
                             </tr>`
-        $('#book_info').append(temp_html)
-        num += 1
-    })
+    $('#book_info').append(temp_html)
+    num += 1
+  })
 }
 
 GetRoom()
--- js/eunsu.js
+++ js/eunsu.js
@@ -1,34 +1,34 @@
 // 기본 URL
-const backend_base_url = "http://ec2-3-39-193-171.ap-northeast-2.compute.amazonaws.com:8000"
+const backend_base_url = "http://127.0.0.1:8000"
 const frontend_base_url = "http://127.0.0.1:5500"
 
 async function RoomviewBySpot(event) {
-    var spotId = event.target.id.slice(-1);
-    const root_address = `${backend_base_url}`; // 여기에 자신의 루트 주소를 입력하세요
-    const fetch_url = `${backend_base_url}/manager/roomsbyspot/` + spotId;
-    const response = await fetch(fetch_url, {});
-    var targetDiv = document.getElementById('contents_id');
+  var spotId = event.target.id.slice(-1);
+  const root_address = `${backend_base_url}`; // 여기에 자신의 루트 주소를 입력하세요
+  const fetch_url = `${backend_base_url}/manager/roomsbyspot/` + spotId;
+  const response = await fetch(fetch_url, {});
+  var targetDiv = document.getElementById('contents_id');
 
-    var dropdown = document.getElementById("navbarDropdown");
-    targetDiv.innerHTML = ''
-    dropdown.textContent = event.target.textContent
+  var dropdown = document.getElementById("navbarDropdown");
+  targetDiv.innerHTML = ''
+  dropdown.textContent = event.target.textContent
 
-    const response_json = await response.json();
+  const response_json = await response.json();
 
 
-    response_json.forEach((a) => {
-        const roomname = a["name"];
-        const price = a["price"];
-        var star = a['avg_star']
-        var starval = "★ x" + star;
-        if (star == 0) {
-            var starval = "아직 별점이 없음";
-        }
-        const image = root_address + a["image"];
-        const roomid = a["id"]
+  response_json.forEach((a) => {
+    const roomname = a["name"];
+    const price = a["price"];
+    var star = a['avg_star']
+    var starval = "★ x" + star;
+    if (star == 0) {
+      var starval = "아직 별점이 없음";
+    }
+    const image = root_address + a["image"];
+    const roomid = a["id"]
 
-        // 변수 하나를 0으로 해서 count ++ 
-        let temp = `< a href = "/review_detail.html?room_id=${roomid}" >
+    // 변수 하나를 0으로 해서 count ++ 
+    let temp = `< a href = "/review_detail.html?room_id=${roomid}" >
                 <section class="cp-card content">
                     <div class="thumb" style="background-image: url(${image});">
                     </div>
@@ -47,8 +47,8 @@ async function RoomviewBySpot(event) {
                     </div>
                 </section>
               </a > `;
-        $("#contents_id").append(temp);
-    });
+    $("#contents_id").append(temp);
+  });
 }
 
 

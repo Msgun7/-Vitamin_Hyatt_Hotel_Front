20da0f59669be32a74972b8ec6687680d69985eb
KimEunSu <misili17114@gmail.com>
2023-05-14 15:59:55 +0900

    Fix: navbar, Add: avg_star

--- js/eunsu.js
+++ js/eunsu.js
@@ -1,25 +1,27 @@
-console.log('자바스크립트 로딩됨 123')
 
+async function RoomviewBySpot(event) {
+  var spotId = event.target.id.slice(-1);
+  const root_address = "http://127.0.0.1:8000"; // 여기에 자신의 루트 주소를 입력하세요
+  const fetch_url = "http://127.0.0.1:8000/manager/roomsbyspot/" + spotId;
+  const response = await fetch(fetch_url, {});
+  var targetDiv = document.getElementById('contents_id');
+  
+  var dropdown = document.getElementById("navbarDropdown");
+  targetDiv.innerHTML = ''
+  dropdown.textContent = event.target.textContent
 
-async function RoomviewBySpot() {
-    const root_address = "http://127.0.0.1:8000"; // 여기에 자신의 루트 주소를 입력하세요
-    const fetch_url = 'http://127.0.0.1:8000/manager/roomsbyspot/' + '1'
-    const response = await fetch(fetch_url, {
-    });
+  const response_json = await response.json();
 
-    const response_json = await response.json();
-    console.log(response_json);
-
-
-    response_json.forEach((a) => {
-        const roomname = a["name"];
-        const price = a["price"];
-        // const star = response_json['avg_star']
-        const image = root_address + a["image"];
-        console.log(image)
-        console.log(roomname);
-
-        let temp = `<a href="/review_detail.html">
+  response_json.forEach((a) => {
+    const roomname = a["name"];
+    const price = a["price"];
+    var star = a['avg_star']
+    var starval = "★ x" + star;
+    if (star == 0) {
+        var starval = "아직 별점이 없음";
+    }
+    const image = root_address + a["image"];
+    let temp = `<a href="/review_detail.html">
                 <section class="cp-card content">
                     <div class="thumb" style="background-image: url(${image});">
                     </div>
@@ -28,6 +30,7 @@ async function RoomviewBySpot() {
                         <div class="metadata">
                             <div class="review-rating">
                                 <span class="cp-stars">
+                                    ${starval}
                                 </span>
                             </div>
                             <div class="review-author">
@@ -37,9 +40,9 @@ async function RoomviewBySpot() {
                     </div>
                 </section>
             </a>`;
-        $("#contents_id").append(temp);
-    });
+    $("#contents_id").append(temp);
+  });
 }
 
 
-RoomviewBySpot();
+
